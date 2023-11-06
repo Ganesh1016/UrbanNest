af5e7b6d82fd9f4cb78628cc46be46523c0bba32
Ganesh1016 <theganeshgajelly02@gmail.com>
2023-11-06 13:20:04 +0530

    Mongo connection added

--- UrbanNestAPI/index.js
+++ UrbanNestAPI/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to UrbanNestDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
