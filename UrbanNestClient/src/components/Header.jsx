63c37981422ff0ad03c6c065d9375c7df06c467b
Ganesh1016 <theganeshgajelly02@gmail.com>
2023-11-11 12:57:30 +0530

    Sign Up form created

--- UrbanNestClient/src/components/Header.jsx
+++ UrbanNestClient/src/components/Header.jsx
@@ -1,31 +1,123 @@
-import { FaSearch } from "react-icons/fa";
+// import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import "./components.css";
+// import "./components.css";
 
 const Header = () => {
   return (
-    <header>
-      <Link to={"/"} className="link">
-        <h1>
-          <span style={{ color: "#427D9D" }}>Urban</span>
-          <span style={{ color: "#363062" }}>Nest</span>
-        </h1>
-      </Link>
-      <form action="">
-        <input type="text" name="" id="" placeholder="Search..." />
-        <FaSearch id="search-icon" />
-      </form>
-      <div className="nav link">
-        <Link to={"/"} className="link">
-          <div className="navigate">Home</div>
-        </Link>
-        <Link to={"/about"} className="link">
-          <div className="navigate">About</div>
-        </Link>
-        <Link to={"/signin"} className="link">
-          <button>Sign In</button>
-        </Link>
-      </div>
+    <header className="w-screen">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to={"/"}>
+            <h1 className="text-3xl font-bold cursor-pointer">
+              <span style={{ color: "#427D9D" }}>Urban</span>
+              <span style={{ color: "#363062" }}>Nest</span>{" "}
+            </h1>
+          </Link>
+
+          <form className="w-2/4">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white bg-blue-500 dark:text-white 
+              focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm absolute end-2.5 bottom-2.5 hover:bg-blue-800 focus:outline-none px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+
+          <div className="flex items-center lg:order-2">
+            <Link
+              to={"/signup"}
+              className="text-white bg-blue-500 dark:text-white 
+              focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm
+              px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700
+              focus:outline-none dark:focus:ring-gray-800"
+            >
+              Get Started
+            </Link>
+
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <Link
+                  to={"/about"}
+                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  About Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
